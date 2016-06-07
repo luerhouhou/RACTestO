168cdbbfbf8e09da1883e7b8eeb5b1505a1d5786
zhang lu <zl_shell@163.com>
2016-06-07 18:44:40 +0800

    Initial Commit

---
+++ RACTestO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RACTestO
+//
+//  Created by zhanglu on 16/6/7.
+//  Copyright © 2016年 zhanglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
